0aac3338111fa9eb5565340973155273a97b1b2e
vergiesky <vergiethie65@gmail.com>
2026-03-03 17:30:04 +0700

    initialize new React project with Vite

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+
+        </Route>
+
+        <Route>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
