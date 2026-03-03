7521505c10f7dfae08728bd7f2a505dcb3daa6ef
vergiesky <vergiethie65@gmail.com>
2026-03-04 04:12:25 +0700

    add user registration

---
+++ src/lib/alert.js
@@ -0,0 +1,17 @@
+import Swal from "sweetalert2";
+
+export const alertSuccess = async (message) => {
+    return Swal.fire({
+        icon: "success",
+        title: "Success",
+        text: message,
+    })
+}
+
+export const alertError = async (message) => {
+    return Swal.fire({
+        icon: "error",
+        title: "Ups",
+        text: message,
+    })
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from "react-router";
+import Layout from "./components/Layout.jsx";
+import UserRegister from "./components/User/UserRegister.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route>
+        <Route element={<Layout/>}>
+            <Route path="/register" element={<UserRegister/>}>
 
+            </Route>
         </Route>
 
-        <Route>
-          
+        <Route path="/dashboard">
+
         </Route>
       </Routes>
     </BrowserRouter>
