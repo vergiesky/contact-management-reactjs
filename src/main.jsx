be6c616196730f4d07e63b92735c276cd40eefb5
vergiesky <vergiethie65@gmail.com>
2026-03-04 19:03:01 +0700

    add user login

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter, Route, Routes} from "react-router";
 import Layout from "./components/Layout.jsx";
 import UserRegister from "./components/User/UserRegister.jsx";
+import UserLogin from "./components/User/UserLogin.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<Layout/>}>
-            <Route path="/register" element={<UserRegister/>}>
-
-            </Route>
+            <Route path="/register" element={<UserRegister/>}/>
+            <Route path="/login" element={<UserLogin/>}/>
         </Route>
 
         <Route path="/dashboard">
