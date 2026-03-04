40e97198e4017444be2ec7aa91844ba76f53629f
vergiesky <vergiethie65@gmail.com>
2026-03-04 20:09:29 +0700

    add user logout

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import UserRegister from "./components/User/UserRegister.jsx";
 import UserLogin from "./components/User/UserLogin.jsx";
 import DashboardLayout from "./components/DashboardLayout.jsx";
 import UserProfile from "./components/User/UserProfile.jsx";
+import UserLogout from "./components/User/UserLogout.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/dashboard" element={<DashboardLayout/>}>
             <Route path="contacts" element={<div>Contacts</div>}/>
             <Route path="users/profile" element={<UserProfile/>}/>
+            <Route path="users/logout" element={<UserLogout/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
